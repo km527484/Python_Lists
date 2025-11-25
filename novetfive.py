bed11a32b913153f7d0c1fb8b689b9995fb73210
km527484 <kamahaseth@gmail.com>
2025-11-25 17:26:33 -0500

    Add files via upload

---
+++ novetfive.py
@@ -0,0 +1,26 @@
+def this_function():
+    return("hello")
+print(this_function())
+
+def add_two(a,b):
+    return a+b
+
+x = add_two(2,3)
+print(x)
+
+def subtract_twonum(a,b):
+    return a - b
+
+def mult_twonum(a,b):
+    return a * b
+
+def div_twonum(a,b):
+    return a / b
+
+y = subtract_twonum(3,2)
+z = mult_twonum (3,2)
+c = div_twonum (4,2)
+
+print(y)
+print(z)
+print(c)
